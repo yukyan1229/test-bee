8716678cbd2b4da5fe17921202448fcea03f4def
yukyan1229 <yuki.okawa@gmail.com>
2026-02-04 04:24:48 +0900

    Wordpress/フッターメニューのレイアウトとデザインを調整

--- cocoon-child-master/tmp-user/footer-custom.php
+++ cocoon-child-master/tmp-user/footer-custom.php
@@ -2,20 +2,21 @@
 // Custom Footer with WP Menu and Copyright
 ?>
 <footer class="custom-footer">
-    <!-- Footer Menu -->
-    <?php
-    wp_nav_menu(array(
-        'theme_location' => 'footer-custom-menu',
-        'container' => 'nav',
-        'container_class' => 'footer-menu-container',
-        'menu_class' => 'footer-menu-items',
-        'fallback_cb' => false, // Do not show default page list if no menu assigned
-    ));
-    ?>
+    <div class="footer-inner">
+        <!-- Copyright -->
+        <div class="footer-copyright">
+            &copy; <?php echo date('Y'); ?> bee-nice.
+        </div>
 
-    <!-- Copyright -->
-    <div class="footer-copyright">
-        &copy;
-        <?php echo date('Y'); ?> bee-nice.
+        <!-- Footer Menu -->
+        <?php
+        wp_nav_menu(array(
+            'theme_location' => 'footer-custom-menu',
+            'container' => 'nav',
+            'container_class' => 'footer-menu-container',
+            'menu_class' => 'footer-menu-items',
+            'fallback_cb' => false, // Do not show default page list if no menu assigned
+        ));
+        ?>
     </div>
 </footer>
